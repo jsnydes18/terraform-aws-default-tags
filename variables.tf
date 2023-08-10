8846306483e66641fb76af3544740fec4ee1e554
jasnyder0421 <jasnyder0721@gmail.com>
2023-08-10 17:24:35 -0500

    fix: remove deprecated quotes on type

--- variables.tf
+++ variables.tf
@@ -1,14 +1,14 @@
 variable "name" {
-    type        = "string"
+    type        = string
     description = "Primary identifier of the resource"
 }
 
 variable "source_project" {
-    type        = "string"
+    type        = string
     description = "Link to source project of the resource"
 }
 
 variable "environment" {
-    type        = "string"
+    type        = string
     description = "Deployment environment of the resourece"
 }
